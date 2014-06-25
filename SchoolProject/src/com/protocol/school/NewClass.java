76979cdaf1b072a844e94b1b27d4c3efb48cd1f4
Protocol <Protocol@Protocol-PC>
2014-06-25 18:30:41 +0530

    hju

--- SchoolProject/src/com/protocol/school/NewClass.java
+++ SchoolProject/src/com/protocol/school/NewClass.java
@@ -7,5 +7,6 @@ public static void main(String args[])
 	System.out.println("Author:Sagar Mahalle");
 	System.out.println("Welcome To Git");
 	System.out.println("Author:Sagar Mahalle");
+	System.out.println("Author:Sagar Mahalle");
 }
 }
