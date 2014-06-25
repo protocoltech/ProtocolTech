74bf24e5c8c40c64244bad6148a8ca630596ad0a
Protocol <Protocol@Protocol-PC>
2014-06-25 18:29:06 +0530

    print name

--- SchoolProject/src/com/protocol/school/NewClass.java
+++ SchoolProject/src/com/protocol/school/NewClass.java
@@ -5,7 +5,7 @@ public static void main(String args[])
 {
 	
 	System.out.println("Author:Sagar Mahalle");
-	
 	System.out.println("Welcome To Git");
+	System.out.println("Author:Sagar Mahalle");
 }
 }
